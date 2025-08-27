ec680d5091d0068737febfd2b97b89b8e1a1b362
Cas0n-McVey <absolgamer25@gmail.com>
2025-08-27 13:59:10 -0500

    E

--- obj/Debug/net8.0/Enums_McVey_Cason.AssemblyInfo.cs
+++ obj/Debug/net8.0/Enums_McVey_Cason.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enums_McVey_Cason")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718be4f06104ff77cb6664744a9b204710d9d103")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enums_McVey_Cason")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enums_McVey_Cason")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
